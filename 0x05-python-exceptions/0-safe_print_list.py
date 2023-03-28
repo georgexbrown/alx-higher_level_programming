55b8ee07b83c1127ab66ef64a8f914654db867c3
George Osondu <browngeorge752@gmail.com>
2023-03-28 02:48:37 +0000

    try-me

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for count in range(x):
         try:
             print(f"{my_list[count]}", end="")
-            count =+ 1
+            count += 1
         except IndexError:
             break
     print()
